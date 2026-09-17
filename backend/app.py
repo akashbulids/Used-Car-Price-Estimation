cff31bc135dd4773955979593777824436085bda
Akash <akashbia251@gmail.com>
2026-09-17 15:52:48 +0530

    Update cors

--- backend/app.py
+++ backend/app.py
@@ -35,6 +35,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://used-car-price-estimation-1.onrender.com"
     ],
 
     allow_credentials=True,
--- frontend_UI/src/App.jsx
+++ frontend_UI/src/App.jsx
@@ -49,7 +49,7 @@ function App() {
     event.preventDefault()
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://used-car-price-estimation.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
